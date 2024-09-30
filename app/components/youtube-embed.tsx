3abe906a0a1e6e94c6b05a0dc6b0f8d0cca621c2
arrlancore <arrlancore@>
2024-09-30 08:16:20 +0700

    feat: youtube auto play

--- app/components/youtube-embed.tsx
+++ app/components/youtube-embed.tsx
@@ -10,7 +10,7 @@ const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ videoId, title }) => {
     <div className="w-full max-w-full aspect-w-16 aspect-h-9">
       <iframe
         className="w-full h-full rounded-lg shadow-lg"
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
         title={title ? title : `YouTube video player`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
